e93256cd95029c92ad49984cde1c0a36374fcbb6
yorkyer <yorkyer93@gmail.com>
2021-11-15 10:39:48 +0800

    fix emplace_back

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,26 +1,33 @@
 #include <benchmark/benchmark.h>
 #include <vector>
 
-struct Object {
-    int x{0};
-    int y{0};
+struct Object
+{
+  int x{0};
+  int y{0};
+  std::vector<int> vec{std::vector<int>(5)};
+  Object(int x, int y) : x(x), y(y) {}
 };
 
-static void BM_push_back(benchmark::State& state) {
+static void BM_push_back(benchmark::State &state)
+{
   // Perform setup here
-  for (auto _ : state) {
+  for (auto _ : state)
+  {
     // This code gets timed
     std::vector<Object> objects;
     objects.push_back(Object{1, 2});
   }
 }
 
-static void BM_emplace_back(benchmark::State& state) {
+static void BM_emplace_back(benchmark::State &state)
+{
   // Perform setup here
-  for (auto _ : state) {
+  for (auto _ : state)
+  {
     // This code gets timed
     std::vector<Object> objects;
-    objects.emplace_back(Object{1, 2});
+    objects.emplace_back(1, 2);
   }
 }
 
@@ -29,4 +36,3 @@ BENCHMARK(BM_push_back);
 BENCHMARK(BM_emplace_back);
 // Run the benchmark
 BENCHMARK_MAIN();
-
