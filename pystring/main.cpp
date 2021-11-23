c2bcaf0cbbdcdc8d6647f86dd256a6dbe7677918
yorkyer <yorkyer93@gmail.com>
2021-11-23 17:37:14 +0800

    update pystring

--- pystring/main.cpp
+++ pystring/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <pystring.h>
+#include <pprint.hpp>
 
 using namespace std;
 
 int main()
 {
+    pprint::PrettyPrinter printer;
+
     cout << boolalpha << pystring::endswith("endswith", "with") << endl;
+    string str = "hello,1,hi";
+    vector<string> result;
+    pystring::split(str, result, ",");
+    printer.print(result);
+    
     return 0;
 }
