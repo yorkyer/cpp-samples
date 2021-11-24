01ee8b1b61fd119b249a9bb087a11c47c3d6b8af
yorkyer <yorkyer93@gmail.com>
2021-11-24 18:15:19 +0800

    update new-sample for pprint and backward-cpp

--- new-sample.sh
+++ new-sample.sh
@@ -17,12 +17,29 @@ cat <<EOF > CMakeLists.txt
 project(${name})
 
 add_executable(\${PROJECT_NAME} main.cpp)
+target_link_libraries(\${PROJECT_NAME} PRIVATE CONAN_PKG::backward-cpp)
 EOF
 
 cat <<EOF > main.cpp
-#include<bits/stdc++.h> 
+#include <bits/stdc++.h> 
+#include <pprint.hpp>
+#include <backward.hpp>
+
+namespace backward
+{
+    backward::SignalHandling sh;
+}
 
 using namespace std;
+
+#define P(...) \\
+do { \\
+    pprint::PrettyPrinter printer; \\
+    printer.quotes(false); \\
+    printer.compact(false); \\
+    printer.print(__VA_ARGS__); \\
+} while (0)
+
 
 int main(int argc, char **argv)
 {
