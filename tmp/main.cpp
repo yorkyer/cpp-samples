68b793affad62620df64858640a6efadef7d2ecc
yorkyer <yorkyer93@gmail.com>
2022-04-19 13:52:02 +0800

    update tmp main

--- tmp/main.cpp
+++ tmp/main.cpp
@@ -1,4 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h> 
+#include <pprint.hpp>
+#include <backward.hpp>
+#include <spdlog/spdlog.h>
+#include <pystring.h>
+#include <mio/mmap.hpp>
+#include <spy/spy.hpp>
+#include <args.hxx>
+#include <tscns.h>
+
+
+namespace backward
+{
+    backward::SignalHandling sh;
+}
+
+
+using namespace std;
+
+
+#define P(...) \
+do { \
+    pprint::PrettyPrinter printer; \
+    printer.quotes(false); \
+    printer.compact(false); \
+    printer.print(__VA_ARGS__); \
+} while (0)
 
 
 int main() {
