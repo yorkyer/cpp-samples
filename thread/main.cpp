6768dfc72bfbbee76e2ceb85b8120d39054b1266
yorkyer <yorkyer93@gmail.com>
2022-06-11 14:05:17 +0800

    update

--- thread/main.cpp
+++ thread/main.cpp
@@ -2,23 +2,44 @@
 
 using namespace std;
 
-void f()
-{
-    int sum = 0;
-    for (int i = 0; i < 1000000000; ++i) {
-        sum += i * i;
-    }
-    cout << sum << endl;
+const size_t num_threads = 4;
+std::condition_variable cv;
+std::mutex mtx;
+
+void g() {
+    int ready_count{};
+    std::vector<std::thread> threads;
+
+    auto f = [&]() {
+        int m = 0;
+        for (int i = 0; i < 1000; ++i) {
+            m += i * i;
+        }
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            ready_count++;
+        }
+        
+        cv.notify_one();
+    };
+
+    threads.emplace_back(f);
+    threads.emplace_back(f);
+    threads.emplace_back(f);
+    threads.emplace_back(f);
+
+    std::unique_lock lk{mtx};
+    cv.wait(lk, [&]{ return ready_count == num_threads; });
+
+    for (auto &t : threads)
+        t.join();
 }
 
 int main(int argc, char **argv)
 {
-    std::vector<std::thread> threads;
-    threads.emplace_back(f);
-    threads.emplace_back(f);
-    threads.emplace_back(f);
-    threads.emplace_back(f);
-
-    for (auto &t : threads) t.join();
+    for (int i = 0; i < 100000; i ++) {
+        std::cout << i << std::endl;
+        g();
+    }
     return 0;
 }
